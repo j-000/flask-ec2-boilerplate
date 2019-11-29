3871784b35bc1fe5a4228ce30debd4c17562828a
J-000 <jjasilva85@gmail.com>
2019-11-29 13:03:40 +0000

    admin, login, deco

--- database.py
+++ database.py
@@ -1,7 +1,10 @@
 from server import db
+from datamodels.Users import User
 
 def main():
   db.create_all()
+  User('Admin', 'Admin@domain.something', 'change-me')
+  
 
 if __name__ == "__main__":
   main()
